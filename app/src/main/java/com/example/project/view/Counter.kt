5ea39c3c33fb24de951fda26806891564822839e
akshay <akshay.mishra@bijnis.com>
2025-09-23 09:38:57 +0530

    ui

--- app/src/main/java/com/example/project/view/Counter.kt
+++ app/src/main/java/com/example/project/view/Counter.kt
@@ -1,82 +1,71 @@
 package com.example.project.view
 
-import androidx.compose.foundation.background
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project.vm.CounterVM
 
 @Composable
-fun CounterUI( viewModel: CounterVM) {
-    val counter = viewModel.counter.collectAsState()
+fun CounterUI(viewModel: CounterVM) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp),
+    val count = viewModel.counter.collectAsState()
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.padding(20.dp).fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Cyan)
-                .padding(20.dp)
         ) {
-            Column (horizontalAlignment = Alignment.CenterHorizontally){
-                Text("${counter.value}")
 
-                Spacer(modifier = Modifier.height(10.dp))
+            Box(modifier = Modifier.fillMaxWidth())
+            {
+                Column(horizontalAlignment = Alignment.CenterHorizontally){
+                Text(
+                    text = "${count.value}",
+                    fontSize = 12.sp,
+                    color = Color.Black
+                )
 
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly,
-                )
-                {
+                ) {
                     Button(
-                         onClick = {
+                        onClick = {
                             viewModel.increaseCount()
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Green,
-                            contentColor = Color.White
-                        ),
-                        ) {
+                        }
+                    ) {
                         Text(
-                            text = "+", fontSize = 32.sp
+                            text = "+",
+                            fontSize = 24.sp,
                         )
                     }
                     Button(
-                        onClick = {viewModel.decrementCount()},
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.Green,
-                            contentColor = Color.White
-                        ),
+                        onClick = {
+                            if(count.value > 0){
+                            viewModel.decrementCount()
+                            }else{
+                                Toast.makeText(context,"Cannot go below 0", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     ) {
                         Text(
-                            text = "-", fontSize = 32.sp
+                            text = "-",
+                            fontSize = 24.sp,
                         )
                     }
                 }

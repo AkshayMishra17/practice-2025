37656b7ad2e5e7682b32d679668ac195ea384fdc
akshay <akshay.mishra@bijnis.com>
2025-10-06 15:43:03 +0530

    sheet added

--- app/src/main/java/com/example/project/view/product/ProductListUI.kt
+++ app/src/main/java/com/example/project/view/product/ProductListUI.kt
@@ -6,16 +6,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -24,15 +31,21 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,6 +65,7 @@ fun ProductListUI(viewModel: ProductVM,navController: NavController) {
     val productList = viewModel.products.collectAsState()
     val error = viewModel.error.collectAsState()
     val isLoading = viewModel.isLoading.collectAsState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         viewModel.fetchProducts()
@@ -71,15 +85,36 @@ fun ProductListUI(viewModel: ProductVM,navController: NavController) {
                 CircularProgressIndicator(modifier = Modifier.size(32.dp))
             }
         }else {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                modifier = Modifier.padding(innerPadding),
-                userScrollEnabled = true
-            ) {
-                items(productList.value) { product ->
-                    ProductCardUI(product,viewModel)
+//            LazyVerticalGrid(
+//                columns = GridCells.Fixed(2),
+//                modifier = Modifier.padding(innerPadding),
+//                userScrollEnabled = true
+//            ) {
+//                items(productList.value) { product ->
+//                    ProductCardUI(product,viewModel)
+//                }
+//            }
+            Column(modifier = Modifier.fillMaxSize().padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+            Button(
+                onClick = {
+                  showBottomSheet = true
                 }
-            }
+            ) {
+                Text(
+                    text = "See Products",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }}
+        }
+        if(showBottomSheet){
+            ProductBottomSheet(
+                viewModel,
+                {showBottomSheet = false}
+            )
         }
     }
 }
@@ -194,6 +229,34 @@ fun ProductListingTopBar(navController: NavController, viewModel: ProductVM) {
                         color = Color.White,
                         fontWeight = FontWeight.Bold,
                     )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductBottomSheet(viewModel: ProductVM, onDismiss: () -> Unit) {
+    val product by viewModel.products.collectAsState()
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        sheetState = sheetState,
+        windowInsets = WindowInsets.ime,
+        dragHandle = null,
+    ) {
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(end = 16.dp) // Ensures last item is visible
+        ) {
+            items(product) { prod ->
+                Box(modifier = Modifier.width(190.dp).height(280.dp)) {
+                    ProductCardUI(prod, viewModel)
                 }
             }
         }
